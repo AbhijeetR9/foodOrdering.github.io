9ec270c916ce8dc6f81d32d38deeae01dc6fb39b
AbhijeetR9 <79084621+AbhijeetR9@users.noreply.github.com>
2021-05-25 23:08:28 +0530

    added submit button id listener

--- contact.js
+++ contact.js
@@ -3,3 +3,9 @@ function submitForm() {
   alert("we Can't Upload the data Because we don't have any dataBase.")
 }
 
+
+document.getElementById("submitbtn").addEventListener('click',()=>{
+    alert("we Can't Upload the data Because we don't have any dataBase.")
+})
+
+
